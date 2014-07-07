5ac09d11489ecbe8ef5e65a45d29229f48c7639e
Isaac Murchie <isaac@saucelabs.com>
2014-07-07 15:59:50 -0700

    Add ConnectionType enum

---
+++ appium/webdriver/connectiontype.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+
+
+"""
+Connection types are specified here:
+    https://code.google.com/p/selenium/source/browse/spec-draft.md?repo=mobile#120
+    Value (Alias)      | Data | Wifi | Airplane Mode
+    -------------------------------------------------
+    0 (None)           | 0    | 0    | 0
+    1 (Airplane Mode)  | 0    | 0    | 1
+    2 (Wifi only)      | 0    | 1    | 0
+    4 (Data only)      | 1    | 0    | 0
+    6 (All network on) | 1    | 1    | 0
+"""
+class ConnectionType(Enum):
+    NO_CONNECTION = 0
+    AIRPLANE_MODE = 1
+    WIFI_ONLY = 2
+    DATA_ONLY = 4
+    ALL_NETWORK_ON = 6

008f6847e473b098bef54bd74bd8203b9befe499
Isaac Murchie <isaac@saucelabs.com>
2014-11-14 08:43:09 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Appium-Python-Client',
-    version='0.10',
+    version='0.11',
     description='Python client for Appium 1.0',
     keywords=[
         'appium',

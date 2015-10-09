8314e07703ea82762f66289513abab8671c09c20
Isaac Murchie <isaac@saucelabs.com>
2015-10-09 08:37:30 -0700

    v0.19

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Appium-Python-Client',
-    version='0.18',
+    version='0.19',
     description='Python client for Appium 1.4',
     keywords=[
         'appium',

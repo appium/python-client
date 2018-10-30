560cda2c91958d755a9ff5768245d942ea998912
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-10-30 10:22:03 +0900

    v0.29

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='Appium-Python-Client',
-    version='0.28',
+    version='0.29',
     description='Python client for Appium 1.5',
     keywords=[
         'appium',

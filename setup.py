258bb0192a492e2fbd71ba09483ea0c25dbdeaf9
Prabhash <pr4bh4sh@users.noreply.github.com>
2018-12-21 23:44:55 +0000

    Fixing broken pypi long description rendering
    reference: https://packaging.python.org/guides/making-a-pypi-friendly-readme
    
    Tested at https://pypi.org/project/delayed-assert

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     version=library_version(),
     description='Python client for Appium',
     long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     keywords=[
         'appium',
         'selenium',

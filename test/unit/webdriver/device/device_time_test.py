83547db11614b86a119354bdd5a61076b40007e0
Atsushi Mori <ki4070ma@gmail.com>
2019-01-05 16:03:26 +0900

    Add device time test

---
+++ test/unit/webdriver/device/device_time_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from test.unit.helper.test_helper import appium_command, android_w3c_driver, ios_w3c_driver
+
+from datetime import datetime as dt
+import httpretty
+
+
+class TestWebDriverDeviceLock(object):
+
+    @httpretty.activate
+    def test_device_time(self):
+        driver = android_w3c_driver()
+        httpretty.register_uri(
+            httpretty.GET,
+            appium_command('/session/1234567890/appium/device/system_time'),
+            body='{"value": "2019-01-05T14:46:44+09:00"}'
+        )
+        dt.strptime(driver.device_time, '%Y-%m-%dT%H:%M:%S%z')  # Expected no exception

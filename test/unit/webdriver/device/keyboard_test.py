91ac778e2d70c195ecfd0903bf582786e3af3ef0
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2019-01-18 18:16:44 +0900

    import keyboard, add tests

---
+++ test/unit/webdriver/device/keyboard_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from test.unit.helper.test_helper import (
+    appium_command,
+    android_w3c_driver,
+    get_httpretty_request_body
+)
+
+import httpretty
+
+from appium.webdriver.webdriver import WebDriver
+
+
+class TestWebDriverKeyboard(object):
+
+    @httpretty.activate
+    def test_hide_keyboard(self):
+        driver = android_w3c_driver()
+        httpretty.register_uri(
+            httpretty.POST,
+            appium_command('/session/1234567890/appium/device/hide_keyboard')
+        )
+        assert isinstance(driver.hide_keyboard(), WebDriver) == True

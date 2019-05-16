ec27cef8005c0c7106023a4cd6efc6c6b68a60d2
Atsushi Mori <ki4070ma@gmail.com>
2019-05-17 08:36:29 +0900

    Add check to ci.sh

--- ci.sh
+++ ci.sh
@@ -7,5 +7,12 @@ if [[ $result ]] ; then
   exit 1
 fi
 
+result=$(python -m isort -rc . | grep -v "Skipped")
+if [[ $result ]] ; then
+  echo $result
+  echo "Please run command 'python -m isort -rc .' on your local and commit the result"
+  exit 1
+fi
+
 python -m pylint --rcfile .pylintrc appium test --errors-only
 python -m pytest test/unit/

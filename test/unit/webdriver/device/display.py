7b5a55861e5512e00fe19c237ad3126830e76e60
Atsushi Mori <ki4070ma@gmail.com>
2019-05-21 10:36:03 +0900

    Add get_display_density unittest

---
+++ test/unit/webdriver/device/display.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from test.unit.helper.test_helper import (
+    android_w3c_driver,
+    appium_command,
+    get_httpretty_request_body
+)
+
+import httpretty
+
+
+class TestWebDriverDisplay(object):
+
+    @httpretty.activate
+    def test_get_display_density(self):
+        driver = android_w3c_driver()
+        httpretty.register_uri(
+            httpretty.GET,
+            appium_command('/session/1234567890/appium/device/display_density'),
+            body='{"value": 560}'
+        )
+        assert driver.get_display_density() == 560

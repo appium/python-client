12a2010fb6429f777d58a45a10112855e4e5d674
Atsushi Mori <ki4070ma@gmail.com>
2019-05-23 22:58:10 +0900

    Keep existing comparison operator

--- setup.py
+++ setup.py
@@ -54,5 +54,5 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing'
     ],
-    install_requires=['selenium<4']
+    install_requires=['selenium >= 3.14.1, < 4']
 )

45629910eba9628c643d9d2d7605ea178e637bfb
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-22 23:29:29 +0900

    try run all scripts and exit 1 when something fails

--- ci.sh
+++ ci.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-
+EXIT_STATUS=0
 if ! python -m autopep8 --exit-code -a -r --global-config .config-pep8 -i . ; then
   echo "Please run command 'python -m autopep8 -a -r --global-config .config-pep8 -i .' on your local and commit the result"
-  exit 1
+  EXIT_STATUS=1
 fi
 
 if ! python -m isort --check-only -rc . ; then
   echo "Please run command 'python -m isort -rc .' on your local and commit the result"
-  exit 1
+  EXIT_STATUS=1
 fi
 
-python -m pylint --rcfile .pylintrc appium test --errors-only
-python -m pytest test/unit/
+(
+  python -m pylint --rcfile .pylintrc appium test --errors-only
+)
+if [ $? -eq 1 ] ; then
+  EXIT_STATUS=1
+fi
+
+(
+  python -m pytest test/unit/
+)
+if [ $? -eq 1 ] ; then
+  EXIT_STATUS=1
+fi
+
+exit $EXIT_STATUS

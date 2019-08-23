29f3d9e1f9b2f81058e30709346152d3ba774dc7
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-23 22:00:59 +0900

    check StopIteration in the output of pylint

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o pipefail
+
 EXIT_STATUS=0
 if ! python -m autopep8 --exit-code -a -r --global-config .config-pep8 -i . ; then
   echo "Please run command 'python -m autopep8 -a -r --global-config .config-pep8 -i .' on your local and commit the result"
@@ -12,15 +14,18 @@ if ! python -m isort --check-only -rc . ; then
 fi
 
 (
-  python -m pylint --rcfile .pylintrc appium test --errors-only
-)
-if [[ $? -eq 1 ]] ; then
-  if [[ "$(python -V)" =~ "Python 3.7" ]] ; then
-    echo "Ignore lint since 'StopIteration' runtime error raised in Python 3.7"
-  else
+  LINT_RESULT=$(python -m pylint --rcfile .pylintrc appium test --errors-only 2>&1 | tee /dev/tty)
+  if [[ $? -eq 1 ]] ; then
     EXIT_STATUS=1
   fi
-fi
+
+  # FIXME: pylint x Python 3.7 cause this runtime error.
+  # We must remove here when we drop Python 2 (and can update pylint) or
+  # install newer pylint for Python 3.7 environment on CI
+  if [[ $LINT_RESULT =~ "RuntimeError: generator raised StopIteration" ]] ; then
+    EXIT_STATUS=0
+  else
+)
 
 (
   python -m pytest test/unit/

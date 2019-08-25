b4096d28a2c4579d0996c29ecd601b66b176591b
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-08-26 00:21:48 +0900

    ci: try run all scripts and exit 1 when something fails (#431)
    * try run all scripts and exit 1 when something fails
    
    * ignore link in Python 3.7 because of runtime error

--- ci.sh
+++ ci.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+set -o pipefail
 
+EXIT_STATUS=0
 if ! python -m autopep8 --exit-code -a -r --global-config .config-pep8 -i . ; then
   echo "Please run command 'python -m autopep8 -a -r --global-config .config-pep8 -i .' on your local and commit the result"
-  exit 1
+  EXIT_STATUS=1
 fi
 
 if ! python -m isort --check-only -rc . ; then
   echo "Please run command 'python -m isort -rc .' on your local and commit the result"
-  exit 1
+  EXIT_STATUS=1
 fi
 
-python -m pylint --rcfile .pylintrc appium test --errors-only
-python -m pytest test/unit/
+(
+  LINT_RESULT=$(python -m pylint --rcfile .pylintrc appium test --errors-only 2>&1 | tee /dev/tty)
+  if [[ $? -ne 0 ]] ; then
+    EXIT_STATUS=1
+  fi
+
+  # FIXME: pylint x Python 3.7 cause this runtime error.
+  # We must remove here when we drop Python 2 (and can update pylint) or
+  # install newer pylint for Python 3.7 environment on CI
+  if [[ $LINT_RESULT =~ "RuntimeError: generator raised StopIteration" ]] ; then
+    EXIT_STATUS=0
+  fi
+)
+
+(
+  python -m pytest test/unit/
+)
+if [[ $? -ne 0 ]] ; then
+  EXIT_STATUS=1
+fi
+
+exit $EXIT_STATUS

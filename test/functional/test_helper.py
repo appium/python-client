917b34fbb1604ac56b6f77409278c8fde1a1d278
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-10-13 07:48:36 +0100

    add format

--- test/functional/test_helper.py
+++ test/functional/test_helper.py
@@ -2,7 +2,7 @@ def get_available_port_range(from_range, to_range):
     """Returns available local port number.
     """
     if not isinstance(from_range, int) or not isinstance(to_range, int):
-        raise ValueError('"{}" and "{}" should be type of int')
+        raise ValueError('"{}" and "{}" should be type of int'.format(from_range, to_range))
 
     import socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)

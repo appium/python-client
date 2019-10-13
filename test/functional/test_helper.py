355d1cc74ecd5f26ae346e959d96fb925cff57e2
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-10-13 06:05:20 +0100

    fix condition flow

--- test/functional/test_helper.py
+++ test/functional/test_helper.py
@@ -1,8 +1,8 @@
 def get_available_port_range(from_range, to_range):
     """Returns available local port number.
     """
-    if isinstance(from_range, int) or isinstance(to_range, int):
-        raise ValueError('from_range and to_range should be type of int')
+    if not isinstance(from_range, int) or not isinstance(to_range, int):
+        raise ValueError('"{}" and "{}" should be type of int')
 
     import socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)

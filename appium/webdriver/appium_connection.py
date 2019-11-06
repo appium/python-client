000e7dda2a56d308f1e06597b59b0049578cd1d4
Mykola Mokhnach <mokhnach@gmail.com>
2019-11-06 10:39:00 +0100

    Sort imports

--- appium/webdriver/appium_connection.py
+++ appium/webdriver/appium_connection.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from selenium.webdriver.remote.remote_connection import RemoteConnection, urllib3
+from selenium.webdriver.remote.remote_connection import (
+    RemoteConnection,
+    urllib3
+)
 
 from appium.common.helper import library_version
-
 
 READ_RETRIES = 3
 

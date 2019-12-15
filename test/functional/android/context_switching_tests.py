8d2ed8ddc998327ced7474c4992e0a2c5e57ce51
Atsushi Mori <ki4070ma@gmail.com>
2019-12-15 17:23:02 +0900

    Skip context switching test

--- test/functional/android/context_switching_tests.py
+++ test/functional/android/context_switching_tests.py
@@ -28,21 +28,25 @@ class ContextSwitchingTests(unittest.TestCase):
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
     def test_contexts_list(self):
+        self.skipTest('Need to replace with apk which has WEBVIEW as context')
         self._enter_webview()
         contexts = self.driver.contexts
         self.assertEqual(2, len(contexts))
 
     def test_move_to_correct_context(self):
+        self.skipTest('Need to replace with apk which has WEBVIEW as context')
         self._enter_webview()
         self.assertEqual('WEBVIEW_io.selendroid.testapp', self.driver.current_context)
 
     def test_actually_in_webview(self):
+        self.skipTest('Need to replace with apk which has WEBVIEW as context')
         self._enter_webview()
         self.driver.find_element_by_css_selector('input[type=submit]').click()
         el = self.driver.find_element_by_xpath("//h1[contains(., 'This is my way')]")
         self.assertIsNot(None, el)
 
     def test_move_back_to_native_context(self):
+        self.skipTest('Need to replace with apk which has WEBVIEW as context')
         self._enter_webview()
         self.driver.switch_to.context(None)
         self.assertEqual('NATIVE_APP', self.driver.current_context)
@@ -54,7 +58,7 @@ class ContextSwitchingTests(unittest.TestCase):
         self.driver.quit()
 
     def _enter_webview(self):
-        btn = self.driver.find_element_by_name('buttonStartWebviewCD')
+        btn = self.driver.find_element_by_accessibility_id('buttonStartWebviewCD')
         btn.click()
         self.driver.switch_to.context('WEBVIEW')
 

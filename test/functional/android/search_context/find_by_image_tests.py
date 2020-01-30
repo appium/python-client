2e4798a9e0edc044ffb775093839981d3056f716
Mori Atsushi <ki4070ma@gmail.com>
2020-01-30 19:41:42 +0900

    Remove unused imports

--- test/functional/android/search_context/find_by_image_tests.py
+++ test/functional/android/search_context/find_by_image_tests.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import base64
-import os
 import unittest
 
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
@@ -23,7 +22,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 from test.functional.android.helper import desired_capabilities
-from test.functional.test_helper import is_ci
 
 from ..helper.test_helper import wait_for_element
 

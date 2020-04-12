34427a15c1ec8426068c5b04801d601b8a0db5d2
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2020-04-12 11:22:20 +0900

    feat: Override send_keys without file upload function (#515)
    * add send_keys_direct
    
    * override send_keys
    
    * tune
    
    * add unittest instead of functional test
    
    * tweak syntax

--- appium/webdriver/webelement.py
+++ appium/webdriver/webelement.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.utils import keys_to_typing
 from selenium.webdriver.remote.command import Command as RemoteCommand
 
 from .extensions.search_context import AppiumWebElementSearchContext
@@ -203,4 +204,19 @@ class WebElement(AppiumWebElementSearchContext):
             'value': [value],
         }
         self._execute(Command.SET_IMMEDIATE_VALUE, data)
+        return self
+
+    # Override
+    def send_keys(self, *value):
+        """Simulates typing into the element.
+
+        Args:
+            value (str): A string for typing.
+
+        Returns:
+            `appium.webdriver.webelement.WebElement`
+        """
+        keys = keys_to_typing(value)
+        self._execute(RemoteCommand.SEND_KEYS_TO_ELEMENT,
+                      {'text': ''.join(keys), 'value': keys})
         return self

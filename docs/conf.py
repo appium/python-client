cb3c2bd41616758b3c6d154a903234d12b61e3a9
Atsushi Mori <ki4070ma@gmail.com>
2020-04-29 23:20:21 +0900

    Remove beta

--- docs/conf.py
+++ docs/conf.py
@@ -18,12 +18,12 @@ sys.path.insert(0, os.path.abspath('../appium'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Python client 1.0 beta'
+project = 'Python client 1.0'
 copyright = '2020, Appium'
 author = 'Appium'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0beta'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------

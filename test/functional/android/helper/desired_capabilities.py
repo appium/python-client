63997a136afbfc77ee270ea2d198742cd0ad7ae4
Atsushi Mori <ki4070ma@gmail.com>
2020-05-10 23:05:18 +0900

    Add ignoreHiddenApiPolicyError

--- test/functional/android/helper/desired_capabilities.py
+++ test/functional/android/helper/desired_capabilities.py
@@ -39,7 +39,8 @@ def get_desired_capabilities(app: Optional[str] = None) -> Dict[str, Any]:
         'disableWindowAnimation': True,
         'autoGrantPermissions': True,
         'language': 'en',
-        'locale': 'US'
+        'locale': 'US',
+        'ignoreHiddenApiPolicyError': True
     }
 
     if app is not None:

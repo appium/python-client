5d95625e74b56a86729c66495f2a506887104300
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-09-30 11:34:48 -0700

    try rc2

--- setup.py
+++ setup.py
@@ -47,5 +47,5 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
     ],
-    install_requires=['selenium == 4.0.0.rc1'],
+    install_requires=['selenium == 4.0.0.rc2'],
 )

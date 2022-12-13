a230f5a9a41421dc034445006ad4be59d896ea15
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-12-13 15:15:56 -0800

    remove clear

--- appium/webdriver/webelement.py
+++ appium/webdriver/webelement.py
@@ -143,18 +143,6 @@ class WebElement(SeleniumWebElement):
 
         return self._execute(RemoteCommand.FIND_CHILD_ELEMENTS, {"using": by, "value": value})['value']
 
-    def clear(self) -> 'WebElement':
-        """Clears text.
-
-        Override for Appium
-
-        Returns:
-            `appium.webdriver.webelement.WebElement`
-        """
-        data = {'id': self.id}
-        self._execute(Command.CLEAR, data)
-        return self
-
     def set_text(self, keys: str = '') -> 'WebElement':
         """Sends text to the element.
 

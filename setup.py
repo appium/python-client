15bcdc687c490e844098b2ffe71a88be72cb5b06
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-05-27 14:13:52 -0700

    chore: set the max selenium deps version

--- setup.py
+++ setup.py
@@ -48,5 +48,5 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
     ],
-    install_requires=['selenium ~= 4.1'],
+    install_requires=['selenium >= 4.1, < 4.10'],
 )

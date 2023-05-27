1fa2ddf937af42603f894cc56911f5788f0a33a6
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2023-05-27 21:16:27 +0000

    ci: add python 11

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Environment :: Console',
         'Environment :: MacOS X',
         'Environment :: Win32 (MS Windows)',

9e43da968d2e4f0d941117a3a6f95dd02150a27d
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-05-16 09:54:42 -0700

    test: fix one test

--- test/functional/android/search_context/find_by_image_tests.py
+++ test/functional/android/search_context/find_by_image_tests.py
@@ -62,7 +62,9 @@ class TestFindByImage(object):
     def test_find_multiple_elements_by_image_just_returns_one(self) -> None:
         wait_for_element(self.driver, AppiumBy.ACCESSIBILITY_ID, 'App')
         image_path = desired_capabilities.PATH('file/find_by_image_success.png')
-        els = self.driver.find_elements_by_image(image_path)
+        with open(image_path, 'rb') as png_file:
+            b64_data = base64.b64encode(png_file.read()).decode('UTF-8')
+        els = self.driver.find_elements(AppiumBy.IMAGE, b64_data)
         els[0].click()
         wait_for_element(self.driver, AppiumBy.ACCESSIBILITY_ID, 'Alarm')
 

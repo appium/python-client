7ac1fd9bcdba2fa29bea8c2f746da30f5420920f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-09-23 23:00:54 -0700

    chore: update release script

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 if [ -z "$PYTHON_BIN_PATH" ]; then
-  PYTHON_BIN_PATH=$(which python3 || which python || true)
+  PYTHON_BIN_PATH=$(which python || true)
 fi
 
 export PYTHON_BIN_PATH

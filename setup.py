bdf6e8676a369479e8ed83f3c2246aedcb7c2c71
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-10-11 22:22:19 -0700

    revert

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
     ],
-    install_requires=['selenium @ git+https://github.com/navin772/selenium.git@appium-python'],
+    install_requires=['selenium ~= 4.12'],
 )

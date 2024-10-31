88ca1e705e2bcb6b9816ebf7791dfcecef7386c1
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-10-30 18:15:56 -0700

    feat: require selenium 4.26+

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
     ],
-    install_requires=['selenium ~= 4.12'],
+    install_requires=['selenium ~= 4.26'],
 )

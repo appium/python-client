ebbf8a7d9330c543458a6441c8bc423440b93ec8
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-08-07 21:39:15 +0900

    add comment

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# FIXME: Remove this setup.py completely
+
 try:
     # Python 3.11+
     import tomllib

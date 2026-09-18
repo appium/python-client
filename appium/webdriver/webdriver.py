1162bab613a3418b6f9b4d2cdb5e41dfebd28ede
Dor Blayzer <59066376+Dor-bl@users.noreply.github.com>
2026-09-19 00:07:39 +0200

    fix(webdriver): normalize orientation value to uppercase in setter (#1308)

--- appium/webdriver/webdriver.py
+++ appium/webdriver/webdriver.py
@@ -458,7 +458,7 @@ class WebDriver(
         """
         allowed_values = ['LANDSCAPE', 'PORTRAIT']
         if value.upper() in allowed_values:
-            self.execute(Command.SET_SCREEN_ORIENTATION, {'orientation': value})
+            self.execute(Command.SET_SCREEN_ORIENTATION, {'orientation': value.upper()})
         else:
             raise WebDriverException("You can only set the orientation to 'LANDSCAPE' and 'PORTRAIT'")
 
